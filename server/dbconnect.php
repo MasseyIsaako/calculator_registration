840a2ff284a5e33b75ea7132b0b741cf23d4079e
MasseyIsaako <lasiisaako@gmail.com>
2018-01-09 13:15:09 +1300

    insert query to database functional
    ToDo:
     - create registration form
     - validate form using php
     - if input valid, push to db else return error messages

--- server/dbconnect.php
+++ server/dbconnect.php
@@ -1,7 +1,10 @@
 <?php
     /**
      * Setting variables to conect through to phpmyadmin db
-     * @var
+     * @var $db_host string - server hostname
+     * @var $db_user string - server username to access db
+     * @var $db_pwd string - server password to access db
+     * @var $db_name string - db name to reference access
      */
     $db_host = "localhost";
     $db_user = "root";
@@ -10,12 +13,21 @@
 
     /**
      * Running MYSQLi connection through variable
-     * @var
+     * @var $dbConnect array - allows database connection 
+     * through authenticating server access via username/password
      */
     $dbConnect = mysqli_connect($db_host, $db_user, $db_pwd, $db_name);
 
     if (mysqli_connect_errno()) {
         echo "<p class='font-white'>Connection to database: " . $db_name . ", returned error: " . mysqli_connect_errno() . "</p>";
     }
+
+    // $sql = "INSERT INTO users VALUES ('Tumema', 'Isaako', 'tumems@isaako.com', '123652321', '42')";
+
+    // if (mysqli_query($dbConnect, $sql)) {
+    //     echo "<p class='font-white'>New record sucessfully added.</p>";
+    // } else {
+    //     echo "Error: " . sql . "<br>" . mysqli_error($dbConnect);
+    // }
 
     // INSERT INTO users VALUES ('Massey', 'Isaako', 'lasiisaako@gmail.com', '0212533235', '25');
